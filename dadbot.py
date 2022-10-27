f821aa5645f9f40f24188abc2982300f97d71050
CJ Spence <cas36@stmarys-ca.edu>
2022-10-27 16:17:06 -0700

    deleted most user mentions, except for the stern replies. added a new reply and data bank of bad words that the bot will reply and interact with.

--- dadbot.py
+++ dadbot.py
@@ -2,12 +2,25 @@ import discord
 import random
 from datetime import datetime   
 
+bad_words = [
+        'fuck',
+        'shit',
+        'bitch',
+        'cunt',
+        'hell',
+        'ass',
+        'damn',
+        'dick',
+        'slut',
+        'whore'
+
+
 dad_intro = [
     'hey',
     'hi'
 ]
 
-dad_funny_face = [
+dad_funny_face =[
     '彡໒(⊙ᴗ⊙)७彡',
     '(✦ ‿ ✦)',
     '໒(⊙ᴗ⊙)७✎▤',
@@ -22,7 +35,8 @@ dad_funny_face = [
 
 dad_silly = [
     'waddup',
-    'whattup'
+    'whattup',
+    'whats up'
 ]
 
 dad_replies = [
@@ -44,29 +58,32 @@ class CJClient(discord.Client):
 
         words = message.content.lower().split()
 
+        if message.content.split()[0].lower() in bad_words:
+            await message.channel.send(f"Watch your languge! {message.author.mention}")
+
         if message.content == "do a funny face" or message.content == "make a funny face":
-            await message.channel.send(f"{random.choice(dad_funny_face)} {message.author.mention}")
+            await message.channel.send(f"{random.choice(dad_funny_face)}")
 
         if words[0] == "shut" and words[1] == "up":
             await message.channel.send(f"Now, {message.author.mention}, I will not tolerate that attitude. I pay the bills here.")
 
         if message.content.split()[0].lower() in dad_intro:
-            await message.channel.send(f"Howdy, {message.author.mention}")
+            await message.channel.send(f"Hey bud!")
         
         if words[0] == "whats" and words[1] == "up":
-            await message.channel.send(f"The sky, {message.author.mention}!")
+            await message.channel.send(f"The sky!")
 
         if message.content.split()[0].lower() in dad_silly:
-            await message.channel.send(f"The sky, {message.author.mention}!")
+            await message.channel.send(f"The sky!")
+        
+        if words[0] == "thank" and words[1] == "you":
+             await message.channel.send(f" No problem.")
+        elif message.content.split()[0].lower() in dad_thanks:
+            await message.channel.send(f" Of course.")
         
         if message.content.split()[0].lower() in dad_replies:
             changed_message = message.content.replace("im", "hi")   
-            await message.channel.send(f"Well {changed_message}, I'm Dad! {message.author.mention}")
-        
-        if words[0] == "thank" and words[1] == "you":
-             await message.channel.send(f" No problem, {message.author.mention}.")
-        elif message.content.split()[0].lower() in dad_thanks:
-            await message.channel.send(f" Of course, {message.author.mention}.")
+            await message.channel.send(f"Well {changed_message}, I'm Dad!")
 
 intents = discord.Intents.default()
 intents.message_content = True
